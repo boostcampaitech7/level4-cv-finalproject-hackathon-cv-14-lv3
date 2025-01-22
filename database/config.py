28de68bb4e0212bbfcb6bdbd1e3879b719e0089b
Namgyu-Youn <yynk2012@gmail.com>
2025-01-23 01:43:36 +0900

    🚚 Rename parser, update markdown

--- database/config.py
+++ database/config.py
@@ -6,7 +6,7 @@ def get_db_args():
     parser = argparse.ArgumentParser(description="Database connection configuration")
     parser.add_argument("password", help="PostgreSQL password")
     parser.add_argument("--dbname", type=str, default=os.getenv("DB_NAME", "sales_data"))
-    parser.add_argument("--user", type=str, default=os.getenv("DB_USER", "sales_user"))
+    parser.add_argument("--user", type=str, default=os.getenv("DB_USER", "gorani"))
     parser.add_argument("--host", type=str, default=os.getenv("DB_HOST", "localhost"))
     parser.add_argument("--port", type=str, default=os.getenv("DB_PORT", "5432"))
 
